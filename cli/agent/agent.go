c1b780c6e7e6e614a6701aef1a97211fb684a1cb
Davide Polonio <davide.polonio@zextras.com>
2023-03-03 14:57:14 +0100

    chore: update copyright

--- cli/agent/agent.go
+++ cli/agent/agent.go
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2023 Zextras srl
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Affero General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Affero General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Affero General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
 // Package main represents the main entrypoint of the whole agent CLI application
 package main
 
@@ -20,8 +38,8 @@ type CLI struct {
 	Config         command.Config         `cmd help:"Manage service-discover configuration"`
 	BootstrapToken command.BootstrapToken `cmd help:"Print the bootstrap-token" name:"bootstrap-token"`
 
-	Version        command.Version        `cmd help:"Show the version of this CLI and of the agent running in the host"`
-	Help           command.Help           `cmd help:"Print the program help"`
+	Version command.Version `cmd help:"Show the version of this CLI and of the agent running in the host"`
+	Help    command.Help    `cmd help:"Print the program help"`
 }
 
 func main() {
@@ -31,7 +49,7 @@ func main() {
 	)
 	s := setup.New()
 	cli := &CLI{
-		Setup: s,
+		Setup:       s,
 		SetupWizard: setup.NewWizardSetup(&s),
 		Config: cmd.Config(
 			os.Stdout,
--- cli/server/command/setup/dependencies.go
+++ cli/server/command/setup/dependencies.go
@@ -1,11 +1,29 @@
+/*
+ * Copyright (C) 2023 Zextras srl
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Affero General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Affero General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Affero General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
 package setup
 
 import (
+	"context"
 	"github.com/Zextras/service-discover/cli/lib/carbonio"
 	"github.com/Zextras/service-discover/cli/lib/exec"
 	"github.com/Zextras/service-discover/cli/lib/systemd"
 	"github.com/Zextras/service-discover/cli/lib/term"
-	"context"
 	"github.com/coreos/go-systemd/v22/dbus"
 	"io"
 	"net"
