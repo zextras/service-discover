ae3386c9e1e1ddeb4d375b9872037874ba69b8a8
Davide Frison <davide.frison@zextras.com>
2024-02-22 15:00:36 +0100

    chore: add build packages follow jenkinsfile structure

---
+++ build_packages.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# SPDX-FileCopyrightText: 2023 Zextras <https://www.zextras.com>
+#
+# SPDX-License-Identifier: GPL-2.0-only
+#
+
+
+docker run --entrypoint=/bin/bash -it -v $(pwd)/artifacts:/artifacts -v $(pwd):/tmp/staging registry.dev.zextras.com/jenkins/pacur/ubuntu-20.04:v2 -c "cp -r /tmp/staging/** /tmp && cd /tmp/staging && yap build ubuntu-focal ."
