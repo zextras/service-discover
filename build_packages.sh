9e7489b6c50488f69fc2addc0034e34ca988b5b9
Davide Frison <davide.frison@zextras.com>
2024-02-22 15:42:55 +0100

    chore: shfmt build_packages.sh :nail_care

--- build_packages.sh
+++ build_packages.sh
@@ -5,9 +5,8 @@
 # SPDX-License-Identifier: GPL-2.0-only
 #
 
-
 docker run --rm --entrypoint=/bin/bash \
--v $(pwd)/artifacts:/artifacts \
--v $(pwd):/tmp/staging \
-registry.dev.zextras.com/jenkins/pacur/ubuntu-20.04:v2 \
--c "cp -r /tmp/staging/** /tmp && cd /tmp/staging && yap build ubuntu-focal ."
+	-v $(pwd)/artifacts:/artifacts \
+	-v $(pwd):/tmp/staging \
+	registry.dev.zextras.com/jenkins/pacur/ubuntu-20.04:v2 \
+	-c "cp -r /tmp/staging/** /tmp && cd /tmp/staging && yap build ubuntu-focal ."
