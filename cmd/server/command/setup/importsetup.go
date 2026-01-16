604694cf97578517b7fefa3fa7dd69731e366e07
Gianluca Boiano <morf3089@gmail.com>
2026-01-16 23:01:56 +0100

    refactor: reduce cognitive complexity in cmd/server/command/setup/importsetup.go
    Split complex importSetup() function into smaller helper methods to improve
    readability and pass SonarQube cognitive complexity checks:
    
    - importSetup() orchestrator (complexity reduced from 37 to <15):
      * Main function now delegates to helper methods
      * Each helper handles a distinct phase of the import setup process
    
    - loadConfigAndValidate():
      * Validates network bindings
      * Loads Zimbra local config
      * Retrieves and validates hostname
    
    - extractAndWriteCertificates():
      * Downloads credentials from LDAP
      * Extracts certificates from encrypted archive
      * Writes CA certificate and key to disk with proper permissions
    
    - generateAndWriteConfig():
      * Generates Consul server configuration
      * Writes configuration files with proper permissions
      * Saves bind address to mutable config
    
    - startServerAndSetupACL():
      * Starts Consul server service
      * Handles both container and systemd environments
      * Creates and sets server ACL token
    
    All tests pass (157 tests) and linting passes with 0 issues.

--- cmd/server/command/setup/importsetup.go
+++ cmd/server/command/setup/importsetup.go
@@ -26,34 +26,82 @@ import (
 //
 //nolint:misspell
 func (s *Setup) importSetup(deps businessDependencies) (formatter.Formatter, error) {
-	networks, err := command.NonLoopbackInterfaces(deps)
+	zimbraHostname, ldapHandler, err := s.loadConfigAndValidate(deps)
 	if err != nil {
 		return nil, err
+	}
+
+	extractedFiles, err := s.extractAndWriteCertificates(deps, ldapHandler)
+	if err != nil {
+		return nil, err
+	}
+
+	err = s.generateAndWriteConfig(deps, zimbraHostname, extractedFiles)
+	if err != nil {
+		return nil, err
+	}
+
+	err = command.AddServiceInLDAP(ldapHandler, zimbraHostname)
+	if err != nil {
+		return nil, err
+	}
+
+	isContainer, err := s.startServerAndSetupACL(deps, zimbraHostname, extractedFiles)
+	if err != nil {
+		return nil, err
+	}
+
+	err = os.WriteFile(s.ConsulHome+"/password", []byte(s.Password), 0400)
+	if err != nil {
+		return nil, err
+	}
+
+	if !isContainer {
+		err = systemd.EnableSystemdUnit(deps.SystemdUnitHandler, serviceDiscoverUnit)
+		if err != nil {
+			return nil, errors.Errorf("unable to enable %s unit: %s", serviceDiscoverUnit, err)
+		}
+	}
+
+	return &formatter.EmptyFormatter{}, nil
+}
+
+func (s *Setup) loadConfigAndValidate(deps businessDependencies) (string, carbonio.LdapHandler, error) {
+	networks, err := command.NonLoopbackInterfaces(deps)
+	if err != nil {
+		return "", nil, err
 	}
 
 	err = command.CheckValidBindingAddress(deps, networks, s.BindAddress)
 	if err != nil {
-		return nil, err
+		return "", nil, err
 	}
 
 	zimbraLocalConfig, err := carbonio.LoadLocalConfig(s.LocalConfigPath)
 	if err != nil {
-		return nil, err
+		return "", nil, err
 	}
 
 	ldapHandler := deps.LdapHandler(zimbraLocalConfig)
 
 	zimbraHostname, err := command.RetrieveZimbraHostname(zimbraLocalConfig, ldapHandler)
 	if err != nil {
-		return nil, err
+		return "", nil, err
 	}
 
 	err = command.CheckHostnameAddress(deps, zimbraHostname)
 	if err != nil {
-		return nil, err
+		return "", nil, err
 	}
 
-	err = command.DownloadCredentialsFromLDAP(ldapHandler, s.ClusterCredential)
+	return zimbraHostname, ldapHandler, nil
+}
+
+func (s *Setup) extractAndWriteCertificates(
+	deps businessDependencies,
+	ldapHandler carbonio.LdapHandler,
+) (map[string][]byte, error) {
+	err := command.DownloadCredentialsFromLDAP(ldapHandler, s.ClusterCredential)
 	if err != nil {
 		return nil, errors.WithMessage(err, "unable to download credentials from LDAP")
 	}
@@ -67,6 +115,7 @@ func (s *Setup) importSetup(deps businessDependencies) (formatter.Formatter, err
 	if err != nil {
 		return nil, errors.Errorf("unable to open %s: %s", clusterCredential.Name(), err)
 	}
+
 	// We calculate the path relative to the root (i.e. without the "/" at the beginning) since this should not be
 	// included in standard tarballs
 	tarballCaFullPath, err := filepath.Rel("/", filepath.Join(config.ConsulHome, command.ConsulCA))
@@ -114,66 +163,78 @@ func (s *Setup) importSetup(deps businessDependencies) (formatter.Formatter, err
 	if err != nil {
 		return nil, err
 	}
+
 	defer os.Remove("/" + localCaKeyFullPath)
 
+	return extractedFiles, nil
+}
+
+func (s *Setup) generateAndWriteConfig(
+	deps businessDependencies,
+	zimbraHostname string,
+	extractedFiles map[string][]byte,
+) error {
 	gossipKey := string(extractedFiles[command.GossipKey])
 
 	consulConfigFile, err := s.generateCertificateAndConfig(deps, zimbraHostname, gossipKey)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
 	consulFileBytes, err := json.MarshalIndent(consulConfigFile, "", "  ")
 	if err != nil {
-		return nil, err
+		return err
 	}
 
 	err = os.WriteFile(s.ConsulFileConfig, consulFileBytes, os.FileMode(0600))
 	if err != nil {
-		return nil, errors.Errorf("unable to save generated configuration file in %s: %s", s.ConsulHome, err)
+		return errors.Errorf("unable to save generated configuration file in %s: %s", s.ConsulHome, err)
 	}
 
 	err = permissions.SetStrictPermissions(deps, s.ConsulFileConfig)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
 	err = command.SaveBindAddressConfiguration(s.MutableConfigFile, s.BindAddress)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
 	err = permissions.SetStrictPermissions(deps, s.MutableConfigFile)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	err = command.AddServiceInLDAP(ldapHandler, zimbraHostname)
-	if err != nil {
-		return nil, err
-	}
+	return nil
+}
 
+func (s *Setup) startServerAndSetupACL(
+	deps businessDependencies,
+	zimbraHostname string,
+	extractedFiles map[string][]byte,
+) (bool, error) {
 	isContainer := command.CheckDockerContainer()
 
 	if isContainer && !testingMode {
 		cmd := exec.CommandContext(context.Background(), "service-discoverd-docker", "server")
 
-		err = cmd.Run()
+		err := cmd.Run()
 		if err != nil {
-			return nil, errors.WithMessage(err, "unable to start service-discoverd server")
+			return isContainer, errors.WithMessage(err, "unable to start service-discoverd server")
 		}
 	} else {
 		err := systemd.StartSystemdUnit(deps.SystemdUnitHandler, serviceDiscoverUnit)
 		if err != nil {
-			return nil, errors.WithMessagef(err, "unable to start %s", serviceDiscoverUnit)
+			return isContainer, errors.WithMessagef(err, "unable to start %s", serviceDiscoverUnit)
 		}
 	}
 
 	aclBootstrapToken := command.ACLTokenCreation{}
 
-	err = json.Unmarshal(extractedFiles[command.ConsulACLBootstrap], &aclBootstrapToken)
+	err := json.Unmarshal(extractedFiles[command.ConsulACLBootstrap], &aclBootstrapToken)
 	if err != nil {
-		return nil, errors.WithMessagef(err, "unable to decode ACL Bootstrap token")
+		return isContainer, errors.WithMessagef(err, "unable to decode ACL Bootstrap token")
 	}
 
 	token, err := command.CreateACLToken(
@@ -183,25 +244,13 @@ func (s *Setup) importSetup(deps businessDependencies) (formatter.Formatter, err
 		aclBootstrapToken.SecretID,
 	)
 	if err != nil {
-		return nil, errors.WithMessage(err, "unable to create ACL policy for this server")
+		return isContainer, errors.WithMessage(err, "unable to create ACL policy for this server")
 	}
 
 	err = command.SetACLToken(deps.CreateCommand, token, aclBootstrapToken.SecretID)
 	if err != nil {
-		return nil, err
+		return isContainer, err
 	}
 
-	err = os.WriteFile(s.ConsulHome+"/password", []byte(s.Password), 0400)
-	if err != nil {
-		return nil, err
-	}
-
-	if !isContainer {
-		err = systemd.EnableSystemdUnit(deps.SystemdUnitHandler, serviceDiscoverUnit)
-		if err != nil {
-			return nil, errors.Errorf("unable to enable %s unit: %s", serviceDiscoverUnit, err)
-		}
-	}
-
-	return &formatter.EmptyFormatter{}, nil
+	return isContainer, nil
 }
